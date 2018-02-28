6be1be4f8ecc1a0dbf5497640e6e45efa8ac5679
exiaohao <exiaohao@gmail.com>
2018-02-28 22:09:56 +0800

    Config auto build to redis

--- server.go
+++ server.go
@@ -17,24 +17,12 @@ type redirectDict struct {
 }
 
 type redirectConfig struct {
-	Redirect []redirectDict `yaml:"REDIRECT"`
-}
-
-func initData() {
-	rConfigContent, _ := ioutil.ReadFile("settings.yml")
-	redirConfig := redirectConfig{}
-	err := yaml.Unmarshal(rConfigContent, &redirConfig)
-
-	if err != nil {
-		log.Fatalf("Failed to load config")
-	}
-	fmt.Println(redirConfig)
+	Default   string         `yaml:"DEFAULT"`
+	Redirects []redirectDict `yaml:"REDIRECT"`
 }
 
 func main() {
 	router := gin.Default()
-
-	initData()
 
 	redisClient, redisErr := redis.Dial("tcp", os.Getenv("REDIS_HOST")+":"+os.Getenv("REDIS_PORT"))
 	if redisErr != nil {
@@ -43,12 +31,24 @@ func main() {
 	redisClient.Do("SELECT", os.Getenv("REDIS_DB"))
 	defer redisClient.Close()
 
+	rConfigContent, _ := ioutil.ReadFile("settings.yml")
+	redirConfig := redirectConfig{}
+	err := yaml.Unmarshal(rConfigContent, &redirConfig)
+
+	if err != nil {
+		log.Fatalf("Failed to load config")
+	}
+	for _, e := range redirConfig.Redirects {
+		fmt.Println(e.Hash, e.Url)
+		redisClient.Do("SET", e.Hash, e.Url)
+	}
+
 	router.GET("/:redirectKey", func(c *gin.Context) {
 		redirectKey := c.Param("redirectKey")
 		// c.String(http.StatusOK, "Hello %s", name)
 		resultURL, err := redis.String(redisClient.Do("GET", redirectKey))
 		if err != nil {
-			c.Redirect(302, "http://caafashion.top/")
+			c.Redirect(302, redirConfig.Default)
 		}
 		c.Redirect(302, resultURL)
 	})
